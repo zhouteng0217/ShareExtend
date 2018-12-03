0909d5b29d1b8cd805f8fd96f46f7cc339472227
zhouteng <zhouteng@cmstop.com>
2018-12-03 21:19:43 +0800

    更新下注释和版本号

--- lib/share_extend.dart
+++ lib/share_extend.dart
@@ -1,10 +1,23 @@
+/// A flutter plugin to share text, image, file with system ui.
+/// It is compatible with both andorid and ios.
+///
+///
+/// A open source authorized by zhouteng [https://github.com/zhouteng0217/ShareExtend](https://github.com/zhouteng0217/ShareExtend).
+
 import 'dart:async';
 import 'package:flutter/services.dart';
 import 'dart:ui';
 
+/// Plugin for summoning a platform share sheet.
 class ShareExtend {
+  /// [MethodChannel] used to communicate with the platform side.
   static const MethodChannel _channel = const MethodChannel('share_extend');
 
+  /// method to share with system ui
+  ///  It uses the ACTION_SEND Intent on Android and UIActivityViewController
+  /// on iOS.
+  /// type  "text", "image" ,"file"
+  ///
   static Future<void> share(String text, String type, String authorities,
       {Rect sharePositionOrigin}) {
     assert(text != null);
