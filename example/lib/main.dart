a6165e5906c6304d23bba2459617b8d9d0af963a
zhouteng <qq1210514049@outlook.com>
2018-12-18 01:40:50 +0800

    将动态权限的检查封装到插件库中

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -3,7 +3,6 @@ import 'dart:io';
 
 import 'package:share_extend/share_extend.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:permission/permission.dart';
 import 'package:path_provider/path_provider.dart';
 
 void main() => runApp(new MyApp());
@@ -44,20 +43,10 @@ class _MyAppState extends State<MyApp> {
               child: new Text("share image"),
             ),
             new RaisedButton(
-              onPressed: () async {
-                File f =
-                    await ImagePicker.pickImage(source: ImageSource.gallery);
-                ShareExtend.share(f.path, "file");
+              onPressed: () {
+                _shareApplicationDocumentsFile();
               },
               child: new Text("share file"),
-            ),
-            new RaisedButton(
-              onPressed: () {
-                if (Platform.isAndroid) {
-                  _shareFileWithPerm();
-                }
-              },
-              child: new Text("share android external storage file"),
             ),
           ],
         )),
@@ -65,44 +54,14 @@ class _MyAppState extends State<MyApp> {
     );
   }
 
-  /// share the external storage file ,first check the permission
-  _shareFileWithPerm() async {
-    if (await _checkPermission()) {
-      _shareFile();
-    } else {
-      var result = await _requestPermission();
-
-      /// to ask for permission
-      if (result == PermissionStatus.allow) {
-        _shareFile();
-      }
-    }
-  }
-
-  /// create a test file for the share example
-  _shareFile() async {
-    String filePath = await _createTestFileToShare();
-    ShareExtend.share(filePath, "file");
-  }
-
-  ///create a test file to share
-  _createTestFileToShare() async {
-    Directory storageDir = await getExternalStorageDirectory();
-    File testFile = new File("${storageDir.path}/flutter/test.txt");
+  ///share the documents file
+  _shareApplicationDocumentsFile() async {
+    Directory dir = await getApplicationDocumentsDirectory();
+    File testFile = new File("${dir.path}/flutter/test.txt");
     if (!await testFile.exists()) {
       await testFile.create(recursive: true);
+      testFile.writeAsStringSync("test for share documents file");
     }
-    testFile.writeAsStringSync("test for share");
-    return testFile.path;
-  }
-
-  _checkPermission() async {
-    List<Permissions> perms =
-        await Permission.getPermissionStatus([PermissionName.Storage]);
-    return perms[0].permissionStatus == PermissionStatus.allow;
-  }
-
-  _requestPermission() async {
-    return await Permission.requestSinglePermission(PermissionName.Storage);
+    ShareExtend.share(testFile.path, "file");
   }
 }
