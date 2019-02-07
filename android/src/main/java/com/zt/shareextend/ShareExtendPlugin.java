ab6200b60a9cb6507b5271866fcb05bf81879c23
Rody Davis <rdavis@unifycrm.com>
2019-02-07 10:33:37 -0500

    Updating Android Dep

--- android/src/main/java/com/zt/shareextend/ShareExtendPlugin.java
+++ android/src/main/java/com/zt/shareextend/ShareExtendPlugin.java
@@ -13,9 +13,9 @@ import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Environment;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
-import android.support.v4.content.FileProvider;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 
 
 import java.io.File;
